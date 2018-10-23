6161d99473045f8b344c8153b6586b33e8cbe796
bhaskar191 <bhaskar.kumbar@wielandsolutions.com>
2018-10-23 13:23:44 +0530

    youTube downloader

---
+++ getvideo/application/config.js
@@ -0,0 +1,25 @@
+/*
+ytubedown - v1.0.0
+Arjun SN
+arjun@wielandsolutions.com
+*/
+
+var config = {
+
+  //Application Title
+  title : "Ytubedown: Download YouTube Videos Free.",
+
+  //Error Page Title
+  errorTitle : "Page not Found (404) - Ytubedown",
+
+  //Application URL
+  url : "http://ytubedown.com",
+
+  //Application Description
+  description : "Free online download YouTube videos, audio quickly in 1080p, 720p, MP4, 3GP, and more.",
+
+  //Footer text
+  footerText : "ytubedown &copy; 2018"
+};
+
+module.exports = config;
